c0a73a52231602c31a3214af4a38bb7b6656f9a4
samirmaassarani <samaassarani@gmail.com>
2025-05-02 14:49:03 +0100

    graphs to visualize all data

--- graphic_visualise.py
+++ graphic_visualise.py
@@ -1,36 +1,20 @@
 from IEL import *
 from matplotlib import pyplot as plt
 import numpy as np
-sequence ='GAAGTGACATGGAGACGTAGGGTATTGAATGAGGG'
+sequence ='GAAGTGACATGGAGACGTAGGGTATTGAATGAGGGATATATATTTAGAGGA  '
 
-landscape = IEL(sequence[:22], toehold=0, conc=1e-9)
+landscape = IEL(sequence[:30], toehold=6, conc=1e-9)
 params_srinivas = Params(9.95, -1.7, 1.2, 2.6, 7.5e7, 3e6)
 
-"""
-dG = landscape.energy_intial(params_srinivas)
-fig, ax = plt.subplots()
-ax.set_title("Energy landscape (initial)")
-ax.set_xlabel("pos")
-ax.set_ylabel("dG")
-ax.plot(landscape.state, dG, 'o-')
-plt.show()"""
-"""
+
 dG = landscape.energy_paper(params_srinivas)
 fig, ax = plt.subplots()
 ax.set_title("Energy landscape (paper)")
 ax.set_xlabel("pos")
 ax.set_ylabel("dG")
 ax.plot(landscape.state, dG, 'o-')
-plt.show()"""
-
-dG = landscape.energy_paper(params_srinivas)
-fig, ax = plt.subplots()
-ax.set_title("Energy landscape (with params/RT)")
-ax.set_xlabel("pos")
-ax.set_ylabel("dG")
-ax.plot(landscape.state, dG, 'o-')
 plt.show()
-"""
+
 dG = landscape.energy_paperRT(params_srinivas)
 fig, ax = plt.subplots()
 ax.set_title("Energy landscape (with params/RT)")
@@ -38,7 +22,6 @@ ax.set_xlabel("pos")
 ax.set_ylabel("dG")
 ax.plot(landscape.state, dG, 'o-')
 plt.show()
-
 
 dG = landscape.energy(params_srinivas)
 k_plus, k_minus = landscape.metropolis(params_srinivas)
@@ -52,7 +35,7 @@ ax.plot(landscape.state, k_minus, label="$k_i^-$")
 ax.legend()
 plt.show()
 
-dG = landscape.energy(params_srinivas)
+dG = landscape.energy_paperRT(params_srinivas)
 k_plus, k_minus = landscape.kawasaki(params_srinivas)
 fig, ax = plt.subplots()
 ax.set_title("Transition rates(kawasaki)")
@@ -73,15 +56,13 @@ ax.set_yscale('log')
 ax.plot(landscape.state, occ)
 plt.show()
 
-params_srinivas = Params(9.95, -1.7, 7.3, 0, 7.5e7, 3e6)
 tmfp = [IEL(sequence, toehold=th, conc=1 ).k_eff(params_srinivas) for th in range(15)]
 fig, ax = plt.subplots()
-ax.set_title("Expected reaction rate constant(chnaged)")
+ax.set_title("Expected reaction rate constant")
 ax.set_xlabel("Toehold length")
 ax.set_xticks(range(len(tmfp)))
 ax.set_ylabel("$k_{eff}$")
 ax.set_yscale('log')
 ax.plot(tmfp, 'o-')
 plt.show()
-"""
 
