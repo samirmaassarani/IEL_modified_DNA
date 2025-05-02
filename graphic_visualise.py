cc288dca343d0f5cc3b08cd3f92ff4ae0f8938b1
samirmaassarani <samaassarani@gmail.com>
2025-05-03 00:09:44 +0100

    improved graphic visualiser

--- graphic_visualise.py
+++ graphic_visualise.py
@@ -1,13 +1,13 @@
 from IEL import *
 from matplotlib import pyplot as plt
 import numpy as np
-sequence ='GAAGTGACATGGAGACGTAGGGTATTGAATGAGGGATATATATTTAGAGGA  '
+sequence ='GAAGTGACAT*GGAGAC*GTAGGGTATTGAATGAGGGATATATATTTAGAGGA'
 
 landscape = IEL(sequence[:30], toehold=6, conc=1e-9)
 params_srinivas = Params(9.95, -1.7, 1.2, 2.6, 7.5e7, 3e6)
 
 
-dG = landscape.energy_paper(params_srinivas)
+dG = landscape.IEL(params_srinivas)
 fig, ax = plt.subplots()
 ax.set_title("Energy landscape (paper)")
 ax.set_xlabel("pos")
@@ -23,7 +23,7 @@ ax.set_ylabel("dG")
 ax.plot(landscape.state, dG, 'o-')
 plt.show()
 
-dG = landscape.energy(params_srinivas)
+dG = landscape.energy_paperRT(params_srinivas)
 k_plus, k_minus = landscape.metropolis(params_srinivas)
 fig, ax = plt.subplots()
 ax.set_title("Transition rates(metro)")
@@ -45,15 +45,6 @@ ax.set_ylabel("rate [$s^{-1}$]")
 ax.plot(landscape.state, k_plus, label="$k_i^+$")
 ax.plot(landscape.state, k_minus, label="$k_i^-$")
 ax.legend()
-plt.show()
-
-occ = landscape.occupancy(params_srinivas)
-fig, ax = plt.subplots()
-ax.set_title("Relative occupancy")
-ax.set_xlabel("pos")
-ax.set_ylabel("rel. occupancy [%]")
-ax.set_yscale('log')
-ax.plot(landscape.state, occ)
 plt.show()
 
 tmfp = [IEL(sequence, toehold=th, conc=1 ).k_eff(params_srinivas) for th in range(15)]
