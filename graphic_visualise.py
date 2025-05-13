a5b26f03ba83ce9ed06a390e644f429a1d659421
samirmaassarani <samaassarani@gmail.com>
2025-05-13 23:21:12 +0100

    fixed RT values in double incumbent, and added new metropolis for the conc of gate

--- graphic_visualise.py
+++ graphic_visualise.py
@@ -2,11 +2,13 @@ from IEL import *
 from matplotlib import pyplot as plt
 import numpy as np
 sequence =  'GAAGTGAATGGAGACGTAGGGTATTGAATGAGGGATATATATTTAGAGGA'
+invader = 'GAAGTGAATGGAGACGTAGGGTATTGAATGAGGGATATATATTTAGAGGA'
 
-landscape = IEL(sequence[:15], toehold=6, conc=1e-6)
-params_srinivas = Params(9.95, -1.7, 1.2, 2.6, 7.5e7, 3e6)
-th=6
+landscape = IEL(sequence[:15],invader, toehold=6, concentration=1e-6)
+params_srinivas = Params(9.95, -1.7, 1.5, 1,-2.0,-1, 7.5e7, 1e7)
+#Params = namedtuple('Params', ['G_init', 'G_bp', 'G_p', 'G_s','G_mm','G_nick', 'k_uni', 'k_bi'])
 
+"""
 dG = landscape.energy_lanscape(params_srinivas)
 fig, ax = plt.subplots()
 ax.set_title("Energy landscape (paper)")
@@ -15,14 +17,14 @@ ax.set_ylabel("dG")
 ax.plot(landscape.state, dG, 'o-')
 plt.show()
 
-dG=landscape.energy_paper_rt_plot(params_srinivas)
+dG=landscape.energy_paper_rt(params_srinivas)
 fig, ax = plt.subplots()
 ax.set_title("Energy landscape (paper)")
 ax.set_xlabel("pos")
 ax.set_ylabel("dG")
 ax.plot(landscape.state, dG, 'o-')
 plt.show()
-"""
+
 dG = landscape.energy_lanscape_rt(params_srinivas)
 k_plus, k_minus = landscape.metropolis(params_srinivas)
 fig, ax = plt.subplots()
@@ -48,7 +50,7 @@ ax.legend()
 plt.show()
 """
 
-tmfp = [IEL(sequence, toehold=th, conc=1e-6 ).k_eff(params_srinivas) for th in range(15)]
+tmfp = [IEL(sequence,invader, toehold=th,concentration=1e-6).k_eff(params_srinivas) for th in range(15)]
 fig, ax = plt.subplots()
 ax.set_title("Expected reaction rate constant")
 ax.set_xlabel("Toehold length")
